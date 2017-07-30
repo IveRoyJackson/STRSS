376d2500fd6085d2f647cd2ad175a63b6d40d9cd
TianZiheng <i@tianziheng.xyz>
2017-07-30 16:30:10 +0800

    Fix bugs.

--- mysql_connect.php
+++ mysql_connect.php
@@ -3,7 +3,7 @@
 function(){  
       
 $db = mysql_connect("localhost","root","123")   
-    or die("连接数据库失败！");  
+    or die("Error!连接数据库失败！");  
   
 mysql_select_db("user")  
     or die ("不能连接到user".mysql_error());  
